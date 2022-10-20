d9e28bc326f05077e3f57ccc770b386428d76173
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-20 11:04:07 -0300

    fix: series component to render by store changes

--- src/app/series/series.component.ts
+++ src/app/series/series.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { Observable } from 'rxjs'
+import { Serie } from '../shared/models/api.model'
+import { selectSeries } from '../state/selectors/series.selector'
+import * as SeriesActions from '../state/actions/series.actions'
 
 @Component({
   selector: 'app-series',
   templateUrl: './series.component.html',
   styleUrls: ['./series.component.css'],
 })
-export class SeriesComponent {
-  constructor() {}
+export class SeriesComponent implements OnInit {
+  series$!: Observable<ReadonlyArray<Serie>>
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(SeriesActions.loadSeries())
+    this.series$ = this.store.select(selectSeries)
+  }
 }
