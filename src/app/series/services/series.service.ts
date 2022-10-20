3f4216d4ed9de20dd4525bf916fe8ef015b495fa
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-20 10:28:01 -0300

    add: series service

---
+++ src/app/series/services/series.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable } from '@angular/core'
+import { map } from 'rxjs'
+import { ApiResponse } from 'src/app/shared/models/api.model'
+import { environment } from 'src/environments/environment'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SeriesService {
+  constructor(private http: HttpClient) {}
+  getSeries() {
+    return this.http
+      .get<ApiResponse>(
+        `
+        https://api.themoviedb.org/3/tv/popular?api_key=${environment.apiKey}&language=en-US
+        `
+      )
+      .pipe(map((data: ApiResponse) => data.results))
+  }
+}
