cd0ce667ae6afdbc0e18fc9236ab8e8ea121a2a2
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-19 22:51:40 -0300

    fix: movies components preview

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core'
+import { Observable } from 'rxjs'
+import { Movie } from './models/movie.model'
+import { MoviesService } from './services/movies.service'
 
 @Component({
   selector: 'app-movies',
@@ -6,9 +9,11 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./movies.component.css'],
 })
 export class MoviesComponent implements OnInit {
-  constructor() {}
+  movies$!: Observable<Movie[]>
+  constructor(private _moviesService: MoviesService) {}
 
   ngOnInit() {
     console.log('on movies')
+    this.movies$ = this._moviesService.getMovies()
   }
 }
