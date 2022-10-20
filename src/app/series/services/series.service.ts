c8ae1c56ceb99323148ecd4db79b12acb0b8bc87
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-20 10:46:37 -0300

    fix: series service to handle two types of data results

--- src/app/series/services/series.service.ts
+++ src/app/series/services/series.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { map } from 'rxjs'
-import { ApiResponse } from 'src/app/shared/models/api.model'
+import { ApiResponse, Serie } from 'src/app/shared/models/api.model'
 import { environment } from 'src/environments/environment'
 
 @Injectable({
@@ -16,6 +16,6 @@ export class SeriesService {
         https://api.themoviedb.org/3/tv/popular?api_key=${environment.apiKey}&language=en-US
         `
       )
-      .pipe(map((data: ApiResponse) => data.results))
+      .pipe(map((data: ApiResponse) => data.results as Serie[]))
   }
 }
