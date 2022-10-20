2dbde10696246490472f9de60bec3629e25bbc9f
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-20 09:52:18 -0300

    add: new movies actions to handle loading and catching errors

--- src/app/state/actions/movies.actions.ts
+++ src/app/state/actions/movies.actions.ts
@@ -1,7 +1,13 @@
 import { createAction, props } from '@ngrx/store'
 import { Movie } from 'src/app/movies/models/movie.model'
 
+export const loadMovies = createAction('[Movies Page] Load Movies')
+
 export const retrievedMoviesList = createAction(
   '[Movies API] Retrieve Movies Success',
   props<{ movies: ReadonlyArray<Movie> }>()
+)
+
+export const errorLoadingMovies = createAction(
+  '[Movies API] Movies Loaded Error'
 )
