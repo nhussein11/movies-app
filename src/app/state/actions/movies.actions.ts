7a31262e6bb36247461c70aad84d0998282aab36
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-20 11:44:12 -0300

    fix: movies actions and  selectors to get single movie by id

--- src/app/state/actions/movies.actions.ts
+++ src/app/state/actions/movies.actions.ts
@@ -3,6 +3,11 @@ import { Movie } from 'src/app/shared/models/api.model'
 
 export const loadMovies = createAction('[Movies Page] Load Movies')
 
+export const loadMovieDetails = createAction(
+  '[Movies Page] Load Movie Detals',
+  props<{ id: number }>()
+)
+
 export const retrievedMoviesList = createAction(
   '[Movies API] Retrieve Movies Success',
   props<{ movies: ReadonlyArray<Movie> }>()
--- src/app/state/selectors/movies.selectors.ts
+++ src/app/state/selectors/movies.selectors.ts
@@ -1,5 +1,8 @@
-import { createFeatureSelector } from '@ngrx/store'
+import { createFeatureSelector, createSelector } from '@ngrx/store'
 import { Movie } from 'src/app/shared/models/api.model'
 
 export const selectMovies =
   createFeatureSelector<ReadonlyArray<Movie>>('movies')
+
+export const selectMovieById = (id: number) =>
+  createSelector(selectMovies, movies => movies.find(movie => movie.id === id))
