8e714257b841352c30897b16404d8b6405d781cd
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-21 09:21:15 -0300

    fix: remove unnecesary movie details action

--- src/app/state/actions/movies.actions.ts
+++ src/app/state/actions/movies.actions.ts
@@ -2,12 +2,6 @@ import { createAction, props } from '@ngrx/store'
 import { Movie } from 'src/app/shared/models/api.model'
 
 export const loadMovies = createAction('[Movies Page] Load Movies')
-
-export const loadMovieDetails = createAction(
-  '[Movies Page] Load Movie Detals',
-  props<{ id: number }>()
-)
-
 export const retrievedMoviesList = createAction(
   '[Movies API] Retrieve Movies Success',
   props<{ movies: ReadonlyArray<Movie> }>()
