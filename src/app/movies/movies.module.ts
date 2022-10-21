9e6c760e73ec345a0d038c3dc0b22e1055bb5ccd
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-21 10:38:11 -0300

    add: card and divider imported from primeng depend

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 import { MoviesListComponent } from './components/movies-list/movies-list.component'
 import { MoviesRoutingModule } from './movies-routing.module'
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component'
+import { PrimengModule } from '../primeng/primeng.module'
 
 @NgModule({
-  imports: [CommonModule, MoviesRoutingModule],
+  imports: [CommonModule, MoviesRoutingModule, PrimengModule],
   declarations: [MoviesListComponent, MovieDetailsComponent],
 })
 export class MoviesModule {}
--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -1,7 +1,8 @@
+import { CardModule } from 'primeng/card'
+import { DividerModule } from 'primeng/divider'
 import { NgModule } from '@angular/core'
 import { TabMenuModule } from 'primeng/tabmenu'
-
 @NgModule({
-  exports: [TabMenuModule],
+  exports: [CardModule, DividerModule, TabMenuModule],
 })
 export class PrimengModule {}
