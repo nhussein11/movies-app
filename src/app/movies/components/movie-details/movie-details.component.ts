a68d4a77df9b3e8b30ceeac238fa93adc2f05306
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-21 09:20:33 -0300

    add: movies details logic

--- src/app/movies/components/movie-details/movie-details.component.ts
+++ src/app/movies/components/movie-details/movie-details.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core'
+import { Store } from '@ngrx/store'
+import { Observable } from 'rxjs'
+import { Movie } from 'src/app/shared/models/api.model'
+import { selectMovieById } from 'src/app/state/selectors/movies.selectors'
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.css']
+  styleUrls: ['./movie-details.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MovieDetailsComponent implements OnInit {
+export class MovieDetailsComponent implements OnChanges {
+  @Input() movieIdSelected!: number
+  movie$!: Observable<Movie>
 
-  constructor() { }
+  constructor(private store: Store) {}
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+    this.movie$ = this.store.select(selectMovieById(this.movieIdSelected))
   }
-
 }
