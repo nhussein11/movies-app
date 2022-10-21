c643c7eada1fa43e63b95170a990555f1fb7dd7d
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-21 09:20:17 -0300

    add: movies details to movies module

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MoviesListComponent } from './components/movies-list/movies-list.component'
 import { MoviesRoutingModule } from './movies-routing.module'
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component'
 
 @NgModule({
   imports: [CommonModule, MoviesRoutingModule],
-  declarations: [MoviesListComponent],
+  declarations: [MoviesListComponent, MovieDetailsComponent],
 })
 export class MoviesModule {}
