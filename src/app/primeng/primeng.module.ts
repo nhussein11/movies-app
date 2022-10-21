186aa8221a97e8a1e829cb836bc6a107b0552272
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-21 11:27:54 -0300

    add: prime ng modules

--- src/app/primeng/primeng.module.ts
+++ src/app/primeng/primeng.module.ts
@@ -1,8 +1,9 @@
+import { ButtonModule } from 'primeng/button'
 import { CardModule } from 'primeng/card'
 import { DividerModule } from 'primeng/divider'
 import { NgModule } from '@angular/core'
 import { TabMenuModule } from 'primeng/tabmenu'
 @NgModule({
-  exports: [CardModule, DividerModule, TabMenuModule],
+  exports: [ButtonModule, CardModule, DividerModule, TabMenuModule],
 })
 export class PrimengModule {}
