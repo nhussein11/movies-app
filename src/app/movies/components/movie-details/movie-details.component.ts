1e8736cb34448d0e6b88203f080f6c399771a77c
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-25 10:19:48 -0300

    add: dynamic dialog logic  to watch trailer button logic

--- src/app/movies/components/movie-details/movie-details.component.ts
+++ src/app/movies/components/movie-details/movie-details.component.ts
@@ -3,28 +3,36 @@ import {
   Component,
   Input,
   OnChanges,
-  OnInit,
   SimpleChanges,
 } from '@angular/core'
 import { Store } from '@ngrx/store'
+import { DialogService } from 'primeng/dynamicdialog'
 import { Observable } from 'rxjs'
 import { Movie } from 'src/app/shared/models/api.model'
 import { selectMovieById } from 'src/app/state/selectors/movies.selectors'
+import { MovieTrailerComponent } from '../movie-trailer/movie-trailer.component'
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [DialogService],
 })
 export class MovieDetailsComponent implements OnChanges {
   @Input() movieIdSelected!: number
   movie$!: Observable<Movie>
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, public dialogService: DialogService) {}
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes)
     this.movie$ = this.store.select(selectMovieById(this.movieIdSelected))
+  }
+
+  watchTrailer() {
+    const ref = this.dialogService.open(MovieTrailerComponent, {
+      header: 'Trailer',
+      width: '70%',
+    })
   }
 }
