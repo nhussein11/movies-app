e77a9f112ce29046e12d829682a81f16f607d2a8
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-25 10:21:10 -0300

    fix: clean movie trailer component

---
+++ src/app/movies/components/movie-trailer/movie-trailer.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+  selector: 'app-movie-trailer',
+  templateUrl: './movie-trailer.component.html',
+  styleUrls: ['./movie-trailer.component.scss'],
+})
+export class MovieTrailerComponent {
+  constructor() {}
+}
