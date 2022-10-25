200206ba7e8f837b87aca38139274e5528619eac
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-25 09:15:26 -0300

    add: carousel to movies list

--- src/app/movies/components/movies-list/movies-list.component.ts
+++ src/app/movies/components/movies-list/movies-list.component.ts
@@ -7,11 +7,29 @@ import * as MoviesActions from '../../../state/actions/movies.actions'
 @Component({
   selector: 'app-movies-list',
   templateUrl: './movies-list.component.html',
-  styleUrls: ['./movies-list.component.css'],
+  styleUrls: ['./movies-list.component.scss'],
 })
 export class MoviesListComponent implements OnInit {
   movies$!: Observable<ReadonlyArray<Movie>>
   movieIdSelected!: number
+
+  responsiveOptions = [
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 2,
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ]
 
   constructor(private store: Store) {}
 
