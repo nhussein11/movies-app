08cd54871da1c920588198f8e25dbbd5f1b4fced
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-26 10:44:34 -0300

    add: refactor to global app state

--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -3,9 +3,19 @@ import { Movie, Serie } from '../shared/models/api.model'
 import { moviesReducer } from './reducers/movies.reducers'
 import { seriesReducer } from './reducers/series.reducers'
 
-export interface AppState {
+export interface MoviesState {
   movies: ReadonlyArray<Movie>
+  selectedMovieId: number | null
+}
+
+export interface SeriesState {
   series: ReadonlyArray<Serie>
+  selectedSerieId: number | null
+}
+
+export interface AppState {
+  movies: MoviesState
+  series: SeriesState
 }
 
 export const ROOT_REDUCERS: ActionReducerMap<AppState> = {
