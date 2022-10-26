c54a177c8fdf28317e66199e149c0eb18fb37b81
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-26 10:45:32 -0300

    fix: movies and series reducers

--- src/app/state/reducers/movies.reducers.ts
+++ src/app/state/reducers/movies.reducers.ts
@@ -1,13 +1,25 @@
 import { createReducer, on } from '@ngrx/store'
 import { Movie } from 'src/app/shared/models/api.model'
 import * as MoviesActions from '../actions/movies.actions'
+import { MoviesState } from '../app.state'
 
-export const initialState: ReadonlyArray<Movie> = []
+export const initialState: MoviesState = {
+  movies: [] as ReadonlyArray<Movie>,
+  selectedMovieId: null,
+}
 
 export const moviesReducer = createReducer(
   initialState,
-  on(
-    MoviesActions.retrievedMoviesList,
-    (state, { movies }): ReadonlyArray<Movie> => [...state, ...movies]
-  )
+  on(MoviesActions.retrievedMoviesList, (state, { movies }): MoviesState => {
+    return {
+      movies: [...state.movies, ...movies],
+      selectedMovieId: null,
+    }
+  }),
+  on(MoviesActions.selectMovie, (state, { movieId }): MoviesState => {
+    return {
+      ...state,
+      selectedMovieId: movieId,
+    }
+  })
 )
--- src/app/state/reducers/series.reducers.ts
+++ src/app/state/reducers/series.reducers.ts
@@ -1,13 +1,24 @@
 import { createReducer, on } from '@ngrx/store'
 import { Serie } from 'src/app/shared/models/api.model'
 import * as SeriesActions from '../actions/series.actions'
+import { SeriesState } from '../app.state'
 
-export const initialState: ReadonlyArray<Serie> = []
-
+export const initialState: SeriesState = {
+  series: [] as ReadonlyArray<Serie>,
+  selectedSerieId: null,
+}
 export const seriesReducer = createReducer(
   initialState,
-  on(
-    SeriesActions.retrieveSeriesList,
-    (state, { series }): ReadonlyArray<Serie> => [...state, ...series]
-  )
+  on(SeriesActions.retrieveSeriesList, (state, { series }): SeriesState => {
+    return {
+      series: [...state.series, ...series],
+      selectedSerieId: null,
+    }
+  }),
+  on(SeriesActions.selectSerie, (state, { serieId }): SeriesState => {
+    return {
+      ...state,
+      selectedSerieId: serieId,
+    }
+  })
 )
