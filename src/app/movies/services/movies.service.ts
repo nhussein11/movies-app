a4c0526835a93fab6fa4ab55049059c98166c2d1
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-26 12:12:12 -0300

    fix: get trailer service typed

--- src/app/movies/services/movies.service.ts
+++ src/app/movies/services/movies.service.ts
@@ -2,7 +2,12 @@ import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { map, Observable, tap } from 'rxjs'
 import { environment } from 'src/environments/environment'
-import { ApiResponse, Movie } from '../../shared/models/api.model'
+import {
+  ApiResponse,
+  ApiVideoResponse,
+  Movie,
+  Video,
+} from '@shared/models/api.model'
 
 @Injectable({
   providedIn: 'root',
@@ -11,26 +16,25 @@ export class MoviesService {
   constructor(private _http: HttpClient) {}
 
   getMovies(): Observable<Movie[]> {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${environment.apiKey}&language=en-US&page=1`
+
     return this._http
-      .get<ApiResponse>(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${environment.apiKey}&language=en-US&page=1`
-      )
+      .get<ApiResponse>(url)
       .pipe(map((data: ApiResponse) => data.results as Movie[]))
   }
+
   getMovieTrailer(movieId: number) {
-    return this._http
-      .get(
-        `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${environment.apiKey}&language=en-US`
-      )
-      .pipe(
-        tap((data: any) => {
-          data.results.forEach((video: any) => {
-            if (video.type === 'Trailer' && video.site === 'YouTube') {
-              console.log(video)
-              return video.key
-            }
-          })
+    const url = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${environment.apiKey}&language=en-US`
+
+    return this._http.get<ApiVideoResponse>(url).pipe(
+      map((data: ApiVideoResponse) => {
+        data.results.forEach((video: Video) => {
+          if (video.type === 'Trailer' && video.site === 'YouTube') {
+            return video.key
+          }
+          return
         })
-      )
+      })
+    )
   }
 }
