5e1b80b4f46d86bc11fbcb5d10f96a482c3507cd
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-26 10:44:59 -0300

    fix: selector to compliance global app state

--- src/app/state/selectors/movies.selectors.ts
+++ src/app/state/selectors/movies.selectors.ts
@@ -1,11 +1,21 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store'
 import { Movie } from 'src/app/shared/models/api.model'
+import { MoviesState } from '../app.state'
 
-export const selectMovies =
-  createFeatureSelector<ReadonlyArray<Movie>>('movies')
+export const selectMoviesState = createFeatureSelector<MoviesState>('movies')
 
-export const selectMovieById = (id: number) =>
-  createSelector(selectMovies, movies => {
-    const movie = movies.find(movie => movie.id === id)
-    return movie ? movie : ({} as Movie)
-  })
+export const selectMovies = createSelector(
+  selectMoviesState,
+  (state: MoviesState) => state.movies
+)
+
+export const selectMovieById = createSelector(
+  selectMoviesState,
+  (state: MoviesState) => state.selectedMovieId
+)
+
+export const selectActiveMovie = createSelector(
+  selectMovies,
+  selectMovieById,
+  (movies, movieId) => movies.find(movie => movie.id === movieId)
+)
