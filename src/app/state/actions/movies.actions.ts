6962a59695243d7c1480e2c0178a53cd83fe87c6
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-27 10:31:23 -0300

    add: movie video actions

--- src/app/state/actions/movies.actions.ts
+++ src/app/state/actions/movies.actions.ts
@@ -2,9 +2,18 @@ import { createAction, props } from '@ngrx/store'
 import { Movie } from '@shared/models/api.model'
 
 export const loadMovies = createAction('[Movies Page] Load Movies')
+
 export const selectMovie = createAction(
   '[Movies Page] Select Movie',
   props<{ movieId: number }>()
+)
+export const loadMovieVideo = createAction(
+  '[Movies Page] Load Movie Video',
+  props<{ movieId: number }>()
+)
+export const retrievedMovieVideo = createAction(
+  '[Movies Page] Retrieved Movie Video',
+  props<{ videoKey: string }>()
 )
 
 export const retrievedMoviesList = createAction(
@@ -14,4 +23,7 @@ export const retrievedMoviesList = createAction(
 
 export const errorLoadingMovies = createAction(
   '[Movies API] Movies Loaded Error'
+)
+export const errorLoadingMovieVideo = createAction(
+  '[Movies API] Movie Video Loaded Error'
 )
