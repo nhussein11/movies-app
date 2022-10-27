123ea8709e680261d0c87f7f5c9374b54152f252
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-27 10:31:09 -0300

    fix: movies service to get correct video key

--- src/app/movies/services/movies.service.ts
+++ src/app/movies/services/movies.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { map, Observable, tap } from 'rxjs'
+import { first } from 'rxjs/operators'
 import { environment } from 'src/environments/environment'
 import {
   ApiResponse,
@@ -23,18 +24,17 @@ export class MoviesService {
       .pipe(map((data: ApiResponse) => data.results as Movie[]))
   }
 
-  getMovieTrailer(movieId: number) {
+  getMovieTrailer(movieId: number): Observable<string> {
     const url = `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${environment.apiKey}&language=en-US`
 
     return this._http.get<ApiVideoResponse>(url).pipe(
-      map((data: ApiVideoResponse) => {
-        data.results.forEach((video: Video) => {
-          if (video.type === 'Trailer' && video.site === 'YouTube') {
-            return video.key
-          }
-          return
-        })
-      })
+      map(({ results }: ApiVideoResponse) => results),
+      map((videos: Video[]) => {
+        return videos.find(
+          (video: Video) => video.type === 'Trailer' && video.site === 'YouTube'
+        )
+      }),
+      map((video: Video | undefined) => video?.key || '')
     )
   }
 }
