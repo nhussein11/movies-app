15ff4119a31668166a35e006774baa184a4dd1bd
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-27 10:33:36 -0300

    fix: moves trailer component

--- src/app/movies/components/movie-trailer/movie-trailer.component.ts
+++ src/app/movies/components/movie-trailer/movie-trailer.component.ts
@@ -1,30 +1,41 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { Store } from '@ngrx/store'
-import { MoviesService } from '@movies/services/movies.service'
-import { selectMovieById } from '@state/selectors/movies.selectors'
+import {
+  selectActiveVideoKey,
+  selectMovieById,
+} from '@state/selectors/movies.selectors'
+import * as MoviesActions from '@state/actions/movies.actions'
+import { map, Observable, Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-movie-trailer',
   templateUrl: './movie-trailer.component.html',
   styleUrls: ['./movie-trailer.component.scss'],
 })
-export class MovieTrailerComponent implements OnInit {
+export class MovieTrailerComponent implements OnInit, OnDestroy {
+  movieIdSubscription!: Subscription
   apiLoaded: boolean = false
+  videoKey$: Observable<string | null> = this.store.select(selectActiveVideoKey)
 
-  constructor(private store: Store, private moviesService: MoviesService) {}
-
-  videoId = 'QIZ9aZD6vs0'
+  constructor(private store: Store) {}
 
   ngOnInit() {
     const movieId = this.store.select(selectMovieById)
-    movieId.subscribe(movieId => {
-      this.moviesService.getMovieTrailer(movieId!).subscribe()
+    this.movieIdSubscription = movieId.subscribe(movieId => {
+      if (movieId) {
+        this.store.dispatch(MoviesActions.loadMovieVideo({ movieId }))
+      }
     })
+
     if (!this.apiLoaded) {
       const tag = document.createElement('script')
       tag.src = 'https://www.youtube.com/iframe_api'
       document.body.appendChild(tag)
       this.apiLoaded = true
     }
+  }
+
+  ngOnDestroy() {
+    this.movieIdSubscription.unsubscribe()
   }
 }
