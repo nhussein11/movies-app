80f928f9de8c8e13e789e404036a82e3093bc9c7
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-31 13:03:11 -0300

    fix: remove unnecessary import on app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component'
 import { StoreModule } from '@ngrx/store'
 import { EffectsModule } from '@ngrx/effects'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
-import { environment } from '../environments/environment'
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
