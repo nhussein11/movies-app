a786f6df28cfd64b03d9cf20c14ac9ba8f0c0273
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-11-01 12:09:39 -0300

    fix: series selector with series state entity

--- src/app/state/selectors/series.selector.ts
+++ src/app/state/selectors/series.selector.ts
@@ -1,5 +1,10 @@
-import { createFeatureSelector } from '@ngrx/store'
+import { createFeatureSelector, createSelector } from '@ngrx/store'
 import { Serie } from '@shared/models/api.model'
+import { SeriesState } from '@state/app.state'
 
-export const selectSeries =
-  createFeatureSelector<ReadonlyArray<Serie>>('series')
+export const selectSeriesState = createFeatureSelector<SeriesState>('series')
+
+export const selectSeries = createSelector(
+  selectSeriesState,
+  (state: SeriesState) => state.series
+)
