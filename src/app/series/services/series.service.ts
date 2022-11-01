97ee2b2c5bc95160c6cbb02d468dd9693615577d
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-11-01 12:10:12 -0300

    fix: series service adding get genres and get series with genres

--- src/app/series/services/series.service.ts
+++ src/app/series/services/series.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
-import { map } from 'rxjs'
-import { ApiResponse, Serie } from '@shared/models/api.model'
+import { forkJoin, map, mergeMap } from 'rxjs'
+import { ApiResponse, Genre, Serie } from '@shared/models/api.model'
 import { environment } from 'src/environments/environment'
 
 @Injectable({
@@ -17,5 +17,31 @@ export class SeriesService {
         `
       )
       .pipe(map((data: ApiResponse) => data.results as Serie[]))
+  }
+
+  getGenres() {
+    return this.http
+      .get(
+        `
+        https://api.themoviedb.org/3/genre/tv/list?api_key=${environment.apiKey}&language=en-US
+        `
+      )
+      .pipe(map((data: any) => data.genres))
+  }
+
+  getSeriesWithGenres() {
+    const series$ = this.getSeries()
+    const genres$ = this.getGenres()
+
+    return forkJoin([series$, genres$]).pipe(
+      map(([series, genres]) => {
+        return series.map((serie: Serie) => {
+          serie.genre_ids = serie.genre_ids.map(genreId => {
+            return genres.find((genre: Genre) => genre.id === genreId).name
+          })
+          return serie
+        })
+      })
+    )
   }
 }
