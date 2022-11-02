6644be1a8cf5a6ff0c64a59e87408ece2fd98b45
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-11-02 11:10:33 -0300

    fix: import prime ng on series module

--- src/app/series/series.module.ts
+++ src/app/series/series.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { SeriesRoutingModule } from './series-routing.module'
 import { SeriesListComponent } from './series-list/series-list.component'
+import { PrimengModule } from '@primeng/primeng.module'
 
 @NgModule({
-  imports: [CommonModule, SeriesRoutingModule],
+  imports: [CommonModule, SeriesRoutingModule, PrimengModule],
   declarations: [SeriesListComponent],
 })
 export class SeriesModule {}
