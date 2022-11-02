8f976db3eb1078e937036e4f891af65afab0039c
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-11-02 12:19:57 -0300

    fix: series module

--- src/app/series/series.module.ts
+++ src/app/series/series.module.ts
@@ -4,9 +4,20 @@ import { SeriesRoutingModule } from './series-routing.module'
 import { SeriesListComponent } from './series-list/series-list.component'
 import { PrimengModule } from '@primeng/primeng.module'
 import { SerieCardComponent } from './serie-card/serie-card.component'
+import { SeriesSearchInputComponent } from './series-search-input/series-search-input.component'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
-  imports: [CommonModule, SeriesRoutingModule, PrimengModule],
-  declarations: [SeriesListComponent, SerieCardComponent],
+  imports: [
+    CommonModule,
+    SeriesRoutingModule,
+    PrimengModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [
+    SeriesListComponent,
+    SerieCardComponent,
+    SeriesSearchInputComponent,
+  ],
 })
 export class SeriesModule {}
