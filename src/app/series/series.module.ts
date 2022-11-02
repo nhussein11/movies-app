8705d2631c330b9e9b34d9c12005a46ea0398dbd
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-11-02 11:25:00 -0300

    add: series card component to series module

--- src/app/series/series.module.ts
+++ src/app/series/series.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common'
 import { SeriesRoutingModule } from './series-routing.module'
 import { SeriesListComponent } from './series-list/series-list.component'
 import { PrimengModule } from '@primeng/primeng.module'
+import { SerieCardComponent } from './serie-card/serie-card.component'
 
 @NgModule({
   imports: [CommonModule, SeriesRoutingModule, PrimengModule],
-  declarations: [SeriesListComponent],
+  declarations: [SeriesListComponent, SerieCardComponent],
 })
 export class SeriesModule {}
