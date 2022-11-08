fd55b42c526b40442fe2e7550f7671ca75046f90
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-11-08 09:47:40 -0300

    add: actors typed

--- src/app/shared/models/api.model.ts
+++ src/app/shared/models/api.model.ts
@@ -1,6 +1,6 @@
 export interface ApiResponse {
   page: number
-  results: Movie[] | Serie[]
+  results: Movie[] | Serie[] | Actor[]
   total_pages: number
   total_results: number
 }
@@ -56,10 +56,49 @@ export interface Video {
   type: string
   official: boolean
 }
+export interface Actor {
+  adult: boolean
+  gender: number
+  id: number
+  known_for: KnownFor[]
+  known_for_department: string
+  name: string
+  popularity: number
+  profile_path: string
+}
+
+export interface KnownFor {
+  adult?: boolean
+  backdrop_path?: string
+  genre_ids: number[]
+  id: number
+  media_type: MediaType
+  original_language: OriginalLanguage
+  original_title?: string
+  overview: string
+  poster_path: string
+  release_date?: Date
+  title?: string
+  video?: boolean
+  vote_average: number
+  vote_count: number
+  first_air_date?: Date
+  name?: string
+  origin_country?: string[]
+  original_name?: string
+}
+
+export enum MediaType {
+  Movie = 'movie',
+  Tv = 'tv',
+}
 
 export enum OriginalLanguage {
   En = 'en',
   Es = 'es',
   Fr = 'fr',
   Ja = 'ja',
+  Ko = 'ko',
+  Tl = 'tl',
+  Tr = 'tr',
 }
