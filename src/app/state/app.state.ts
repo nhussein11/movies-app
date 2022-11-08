e8cdb01d9a2d0a014fdb79e9aeb68e22a4226e67
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-11-08 09:48:25 -0300

    fix: global app state adding actors state

--- src/app/state/app.state.ts
+++ src/app/state/app.state.ts
@@ -1,5 +1,6 @@
 import { ActionReducerMap } from '@ngrx/store'
-import { Movie, Serie } from '@shared/models/api.model'
+import { Actor, Movie, Serie } from '@shared/models/api.model'
+import { actorsReducer } from './reducers/actors.reducer'
 import { moviesReducer } from './reducers/movies.reducers'
 import { seriesReducer } from './reducers/series.reducers'
 
@@ -13,13 +14,18 @@ export interface SeriesState {
   series: ReadonlyArray<Serie>
   selectedSerieId: number | null
 }
-
+export interface ActorsState {
+  actors: ReadonlyArray<Actor>
+  selectedActorId: number | null
+}
 export interface AppState {
   movies: MoviesState
   series: SeriesState
+  actors: ActorsState
 }
 
 export const ROOT_REDUCERS: ActionReducerMap<AppState> = {
   movies: moviesReducer,
   series: seriesReducer,
+  actors: actorsReducer,
 }
