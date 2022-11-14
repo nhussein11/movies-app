31b1bd7592f581006d7cd4c21ea566c127ec017a
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-11-13 21:04:22 -0300

    fix: actor list styles

--- src/app/actors/actors-list/actors-list.component.ts
+++ src/app/actors/actors-list/actors-list.component.ts
@@ -4,16 +4,49 @@ import { Actor } from '@shared/models/api.model'
 import { Observable } from 'rxjs'
 import * as ActorsActions from '@state/actions/actors.actions'
 import { selectActors } from '@state/selectors/actors.selector'
+
+//Animation
+import { trigger, state, style, animate, transition } from '@angular/animations'
+import { DialogService } from 'primeng/dynamicdialog'
+import { ActorDetailsComponent } from '../actor-details/actor-details/actor-details.component'
+
+const openDetails = trigger('openDetails', [
+  state(
+    'open',
+    style({
+      opacity: 1,
+      background: 'red',
+    })
+  ),
+  state(
+    'closed',
+    style({
+      opacity: 0,
+    })
+  ),
+  transition('open => closed', [
+    animate('4000ms ease-out'),
+    style({ opacity: 0 }),
+  ]),
+  transition('closed => open', [
+    animate('4000ms ease-in'),
+    style({ opacity: 1 }),
+  ]),
+])
+
 @Component({
   selector: 'app-actors',
   templateUrl: './actors-list.component.html',
-  styleUrls: ['./actors-list.component.css'],
+  styleUrls: ['./actors-list.component.scss'],
+  animations: [openDetails],
+  providers: [DialogService],
 })
 export class ActorsListComponent implements OnInit {
   actors$!: Observable<ReadonlyArray<Actor>>
   actorSelected!: Actor
+  openDetails: string = 'closed'
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private dialogService: DialogService) {}
 
   ngOnInit() {
     this.store.dispatch(ActorsActions.loadActors())
@@ -21,8 +54,11 @@ export class ActorsListComponent implements OnInit {
   }
 
   selectActor(actor: Actor) {
-    this.actorSelected === actor
-      ? (this.actorSelected = {} as Actor)
-      : (this.actorSelected = actor)
+    this.dialogService.open(ActorDetailsComponent, {
+      data: actor,
+      header: actor.name,
+      width: '40%',
+      styleClass: 'actor-details',
+    })
   }
 }
