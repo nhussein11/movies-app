dab93fe5d6e5d7b44da1dac0b36a0d226038631b
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-11-13 21:04:12 -0300

    fix: actor details styles

--- src/app/actors/actor-details/actor-details/actor-details.component.ts
+++ src/app/actors/actor-details/actor-details/actor-details.component.ts
@@ -1,24 +1,16 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  OnChanges,
-  SimpleChanges,
-} from '@angular/core'
+import { Component } from '@angular/core'
 import { Actor } from '@shared/models/api.model'
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog'
 
 @Component({
   selector: 'app-actor-details',
   templateUrl: './actor-details.component.html',
-  styleUrls: ['./actor-details.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./actor-details.component.scss'],
 })
-export class ActorDetailsComponent implements OnChanges {
-  @Input() actor!: Actor
-  constructor() {
-    // console.log(this.actor)
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(changes['actor'].currentValue)
-  }
+export class ActorDetailsComponent {
+  actor: Actor = this.config.data
+  constructor(
+    public ref: DynamicDialogRef,
+    public config: DynamicDialogConfig
+  ) {}
 }
